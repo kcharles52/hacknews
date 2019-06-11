873935443aea50bd81556ddcc61303c8d8005fc3
Charles Kato <kcharles52@gmail.com>
2019-06-11 18:19:27 +0300

    fetch data from API

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
 import Link from './Link'
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
 
 class LinkList extends Component {
-  render() {
-    const linksToRender = [      {
-      id: '1',
-      description: 'Prisma turns your database into a GraphQL API 😎',
-      url: 'https://www.prismagraphql.com',
-    },
+
+  FEED_QUERY = gql`
     {
-      id: '2',
-      description: 'The best GraphQL client',
-      url: 'https://www.apollographql.com/docs/react/',
-    },]
+      feed {
+        links {
+          id
+          createdAt
+          url
+          description
+        }
+      }
+    }
+  `
+  render() {
 
     return (
-      <div>
-        {linksToRender.map(link => <Link key={link.id} link={link}/>)}
-      </div>
+      <Query query={this.FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>
+          if (error) return <div>Error</div>
+          
+          const linksToRender = data.feed.links
+
+          return (
+            <div>
+              {linksToRender.map(link => <Link key={link.id} link={link} />)}
+            </div>
+          )}}
+      </Query>
     )
   }
 }
