5de0bfab140edb7ec5efb793bee31e948b2f9277
Charles Kato <kcharles52@gmail.com>
2019-06-13 14:58:36 +0300

    - add login route - add login component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,9 @@ import { Switch, Route } from "react-router";
 import LinkList from "./LinkList";
 import CreateLink from "./CreateLink";
 import Header from "./Header";
+import Login from "./Login";
+
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +16,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </div>
