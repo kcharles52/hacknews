110ff61818efc36975c18d027c7dd6f670078347
Charles Kato <kcharles52@gmail.com>
2019-06-13 15:50:41 +0300

    - add apollo link context - add postby id to post mutation

--- src/index.js
+++ src/index.js
@@ -11,15 +11,29 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from 'apollo-link-context';
+
+import { AUTH_TOKEN } from "./constants";
 
 // httplink to connect apolloclient to the GraphQL API
 const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
 
+// apollo link middleware
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers:{
+      ...headers, 
+      authorization: token? `Bearer ${token}` : ''
+    }
+  }
+})
+
 // Apolloclient Instance
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
