37e279f6f03103d00efc249dae3c8707d25c9991
Charles Kato <kcharles52@gmail.com>
2019-06-14 16:59:52 +0300

    - subscribtion to posted links

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { setContext } from "apollo-link-context";
 import { split } from "apollo-link";
 import { WebSocketLink } from "apollo-link-ws";
-import { getMainDefinition } from "apollo-utiilities";
+import { getMainDefinition } from "apollo-utilities";
 
 import { AUTH_TOKEN } from "./constants";
 
@@ -36,11 +36,11 @@ const authLink = setContext((_, { headers }) => {
 
 // apollo websocket
 const wsLink = new WebSocketLink({
-  uri:`ws://localhost:400`,
+  uri:`ws://localhost:4000`,
   options: {
     reconnect: true,
     connectionParams: {
-      authToken: localStorage.getItme(AUTH_TOKEN),
+      authToken: localStorage.getItem(AUTH_TOKEN),
     }
   }
 })
